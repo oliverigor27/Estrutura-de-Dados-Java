a3051021246169b80def3119809af7b404cd36fe
Igor Oliveira <igoroliveira9827@gmail.com>
2022-10-12 14:34:31 -0300

    Update archives

---
+++ Busca Binária/BuscaBinaria.java
@@ -0,0 +1,20 @@
+public class BuscaBinaria {
+    private int[] novoArr; 
+    private int inicio, fim;
+    private int result;
+
+    public BuscaBinaria(int dta[]) {
+        this.novoArr = dta;
+        this.inicio = 0;
+        this.fim = novoArr.length - 1;
+        this.result = -1;
+    }
+
+    public int[] serach(int value) {
+        
+        while(this.inicio <= this.fim) {
+            final int media = (this.inicio + this.fim) / 2;
+
+        }
+    }
+}
---
+++ Hashtable/Hashtable.java
@@ -0,0 +1,25 @@
+package Hashtable;
+
+class Hashtable {
+    private int[] vetor;
+    private int size;
+
+    public Hashtable() {
+        this.vetor = new int[100];
+        this.size = 0;
+    }
+
+    private int hash(int[] data) {
+        int hash = 0;
+        int result, i;
+
+        for(i = 0; i < data.length; i++) {
+            
+        }
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
