daa8511901b69b32735ea97949a7ddfab560b319
Igor Oliveira <igoroliveira9827@gmail.com>
2022-10-25 16:26:55 -0300

    Update BuscaBinaria.java

--- Busca Binária/BuscaBinaria.java
+++ Busca Binária/BuscaBinaria.java
@@ -3,18 +3,20 @@ public class BuscaBinaria {
     private int inicio, fim;
     private int result;
 
-    public BuscaBinaria(int dta[]) {
-        this.novoArr = dta;
+    public BuscaBinaria(int[] vet, int dta) {
+        this.novoArr = vet;
         this.inicio = 0;
         this.fim = novoArr.length - 1;
         this.result = -1;
     }
 
-    public int[] serach(int value) {
+    public int[] serach(int value, int dta) {
         
         while(this.inicio <= this.fim) {
             final int media = (this.inicio + this.fim) / 2;
+            if(this.novoArr[media] == dta) {
 
+            }
         }
     }
 }
